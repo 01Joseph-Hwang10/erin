6f0df5490fb52f6f0d0712f7013de06750cf61bb
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-07-17 03:13:16 +0900

    minor file fixes

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,7 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
+
+// eslint-disable-next-line
 const { getDefaultConfig } = require('expo/metro-config');
 
+// eslint-disable-next-line
 module.exports = getDefaultConfig(__dirname);
